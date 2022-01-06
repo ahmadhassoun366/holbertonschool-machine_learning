2f91040fccbeebffd848c38d04dda398291fb43d
Live session user <ahmadhss366@gmail.com>
2022-01-06 23:55:59 +0000

    First one

---
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+	"""norm_constants Module"""
+	
+	import numpy as np
+	
+	
+	def normalization_constants(X):
+	"""Function that returns the mean & standard deviation"""
+	mean = np.mean(X, axis=0)
+	stdev = np.std(X, axis=0)
+	return mean, stdev
+
+
