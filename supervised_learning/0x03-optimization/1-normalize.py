13fd929c12e5bb2bb28f6729614b9b0f2a0ade83
ahmadhassoun366 <72111331+ahmadhassoun366@users.noreply.github.com>
2022-01-14 11:22:27 +0000

    Update 1-normalize.py

--- supervised_learning/0x03-optimization/1-normalize.py
+++ supervised_learning/0x03-optimization/1-normalize.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-	"""normalization Module"""
-	
-	import numpy as np
-	
-	
-	def normalize(X, m, s):
-	"""Function to normalize a matrix"""
-	X = (X - m) / s
-	return X
+"""normalization Module"""
+
+import numpy as np
+
+
+def normalize(X, m, s):
+    """Function to normalize a matrix"""
+    X = (X - m) / s
+    return X
