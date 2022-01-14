0889ad5988228a19c9841d9ecb9c9911f5cf4dce
ahmadhassoun366 <72111331+ahmadhassoun366@users.noreply.github.com>
2022-01-14 10:57:10 +0000

    Update 0-norm_constants.py

--- supervised_learning/0x03-optimization/0-norm_constants.py
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -1,11 +1,8 @@
- 	#!/usr/bin/env python3
-	"""norm_constants Module"""
-	
-	import numpy as np
-	
-	
-	def normalization_constants(X):
-	"""Function that returns the mean & standard deviation"""
-	mean = np.mean(X, axis=0)
-	stdev = np.std(X, axis=0)
-	return mean, stdev
+#!/usr/bin/env python3
+"""norm_constants Module"""
+import numpy as np
+def normalization_constants(X):
+"""Function that returns the mean & standard deviation"""
+mean = np.mean(X, axis=0)
+stdev = np.std(X, axis=0)
+return mean, stdev
